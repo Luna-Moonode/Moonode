608b4535c6f92f71ce0f6df4845744b4e552628c
Luna Moonode <872178273@qq.com>
2019-09-27 00:19:11 +0800

    all

--- Studio-TaskNo.1/venv/lib/ToolBox/main.py
+++ Studio-TaskNo.1/venv/lib/ToolBox/main.py
@@ -1,30 +1,43 @@
+# coding=utf-8
 print("Welcome to the earth!\nThere're three functions:")
 print("1---Base64")
 print("2---Dictionary_Reverse")
 print("3---QRcode_Transfer")
-try:
-    choice=eval(input("Please input the number before a function:"))
-except:
-    print("Invalid input! Program terminated.")
-else:
-    if choice==1:
-        from Base64.Base64 import*
-        try:
-            cho=eval(input("1.加密\n2.解密\n"))
-        except:
-            print("Invalid input! Program terminated.")
-        else:
-            if cho==1:
-                base64encode()
-            elif cho==2:
-                base64decode()
-            else:
-                print("Invalid input! Program terminated.")
-    elif choice==2:
-        from Dict_Reverse.Dict_Reverse import*
-        dict_reverse()
-    elif choice==3:
-        from Qrcode.QRcode import*
-        QRcode()
-    elif choice not in [1,2,3]:
-        print("Invalid input! Program terminated.")
+while True:
+    try:
+        choice=input("Please input the number before a function(q to quit):")
+    except:
+        print("Invalid input! Try again!")
+    else:
+        if choice=='1':
+            from Base64.Base64 import*
+            while True:
+                try:
+                    cho=eval(input("1.加密\n2.解密\n"))
+                except:
+                    print("Invalid input! Try again!")
+                    continue
+                else:
+                    if cho==1:
+                        base64encode()
+                        break
+                    elif cho==2:
+                        base64decode()
+                        break
+                    else:
+                        print("Invalid input! Try again!")
+                        continue
+        elif choice=='2':
+            from Dict_Reverse.Dict_Reverse import*
+            dict_reverse()
+            continue
+        elif choice=='3':
+            from Qrcode.QRcode import*
+            QRcode()
+            continue
+        elif choice=='q':
+            break
+        elif choice not in [1,2,3,q]:
+            print("Invalid input! Try again!")
+            continue
+print ("Program terminated.")
